ad9a20eb005076e4a2ba2a4fd15a6b1287c116e7
SooYou <souyoou@googlemail.com>
2015-10-02 22:55:09 +0200

    emit advance event when booth is empty

--- src/controllers/waitlist.js
+++ src/controllers/waitlist.js
@@ -12,6 +12,7 @@ export const getWaitlist = function getWaitlist(redis) {
 
 export const joinWaitlist = function joinWaitlist(moderatorID, id, position, forceJoin, uwave) {
   const User = uwave.mongo.model('User');
+  const History = uwave.mongo.model('History');
   let beforeID = null;
 
   return uwave.redis.get('waitlist:lock')
@@ -44,27 +45,37 @@ export const joinWaitlist = function joinWaitlist(moderatorID, id, position, for
     return uwave.redis.lrange('waitlist', 0, -1);
   })
   .then(waitlist => {
-    return new Promise((resolve, reject) => {
-      for (let i = waitlist.length - 1; i >= 0; i--) {
-        if (waitlist[i] === id) {
-          if (moderatorID !== id) {
-            uwave.redis.publish('v1', createCommand('waitlistAdd', {
-              'userID': id,
-              'moderatorID': moderatorID,
-              'position': i || waitlist.length - 1,
-              'waitlist': waitlist
-            }));
-          } else {
-            uwave.redis.publish('v1', createCommand('waitlistJoin', {
-              'userID': id,
-              'waitlist': waitlist
-            }));
-          }
-          return resolve(waitlist);
+    for (let i = waitlist.length - 1; i >= 0; i--) {
+      if (waitlist[i] === id) {
+        if (moderatorID !== id) {
+          uwave.redis.publish('v1', createCommand('waitlistAdd', {
+            'userID': id,
+            'moderatorID': moderatorID,
+            'position': i || waitlist.length - 1,
+            'waitlist': waitlist
+          }));
+        } else {
+          uwave.redis.publish('v1', createCommand('waitlistJoin', {
+            'userID': id,
+            'waitlist': waitlist
+          }));
         }
+
+        return new Promise((resolve, reject) => {
+          if (waitlist.length === 1) {
+            History.find().sort({'played': -1}).limit(1).populate('media')
+            .then(lastPlayed => {
+              if (!lastPlayed.length || Date.now() - (lastPlayed.played + lastPlayed.media.duration * 1000) >= 0) {
+                uwave.redis.publish('v1p', createCommand('advance', null));
+                waitlist.shift();
+              }
+              resolve(waitlist);
+            });
+          }
+        });
       }
-      reject(new Error(`couldn't add user ${id} to waitlist`));
-    });
+    }
+    throw new Error(`couldn't add user ${id} to waitlist`);
   });
 };
 
