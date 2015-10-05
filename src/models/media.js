f157bf5f7f4c2a2c5890bfcf5cb34a41e78ad53b
SooYou <souyoou@googlemail.com>
2015-10-05 21:20:46 +0200

    add reference to GlobalMedia

--- src/models/media.js
+++ src/models/media.js
@@ -2,6 +2,7 @@ export default function(mongoose) {
   const Schema = mongoose.Schema;
 
   const mediaSchema = new Schema({
+    'global': { 'type': Schema.Types.ObjectId, 'ref': 'GlobalMedia', 'required': true },
     'artist': { 'type': String, 'max': 128, 'required': true },
     'title': { 'type': String, 'max': 128, 'required': true },
     'start': { 'type': Number, 'min': 0, 'default': 0 },
