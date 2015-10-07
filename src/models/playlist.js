00869f73d961a4d2e602addde586b6dc1ff2ebe7
SooYou <souyoou@googlemail.com>
2015-10-07 20:29:36 +0200

    make description optional

--- src/models/playlist.js
+++ src/models/playlist.js
@@ -4,11 +4,11 @@ export default function(mongoose) {
   const playlistSchema = new Schema({
     'created': { 'type': Date, 'default': Date.now },
     'name': { 'type': String, 'min': 0, 'max': 128, 'required': true },
-    'description': { 'type': String, 'min': 0, 'max': 512, 'required': true },
+    'description': { 'type': String, 'min': 0, 'max': 512 },
     'author': { 'type': Schema.Types.ObjectId, 'ref': 'User', 'required': true },
     'shared': { 'type': Boolean, 'default': false },
     'nsfw': { 'type': Boolean, 'default': false },
-    'media': [{ 'type': Schema.Types.ObjectId, 'ref': 'Media' }]
+    'media': [{ 'type': Schema.Types.ObjectId, 'ref': 'PlaylistItem' }]
   }, {
     'minimize': false
   });
