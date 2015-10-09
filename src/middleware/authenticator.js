edc4f7af23db7ce78c0dacb58455d155d05bca00
SooYou <souyoou@googlemail.com>
2015-10-09 21:29:40 +0200

    extend regex to include all whitelisted routes needed for guests

--- src/middleware/authenticator.js
+++ src/middleware/authenticator.js
@@ -3,18 +3,19 @@ import jwt from 'jsonwebtoken';
 import debug from 'debug';
 
 const verify = bluebird.promisify(jwt.verify);
-const noauth = RegExp(/\/auth\/(login|register|password.*|now)$/, 'i');
+const rx = RegExp(/\/auth\/(login|register|password.*)|\/waitlist|\/booth|\/now|\/(playlists|users)\/[a-f0-9]{24}$/, 'i');
 
 const log = debug('uwave:v1:authenticator');
 
 export default function _authenticator (v1) {
   return function authenticator(req, res, next) {
-    if (noauth.test(req.path)) return next();
+    // check for routes that need no authentication
+    if (rx.test(req.path)) return next();
     if (!req.query.token) return res.status(422).json('no token set');
 
     verify(req.query.token, v1.getCert())
     .then(user => {
-      if (!Object.keys(user).length) return res.status(404).json('user not found. Access token expired?');
+      if (!user) res.status(404).json('user not found');
       if (typeof user.role !== 'number') user.role = parseInt(user.role, 10);
       if (user.role === NaN) user.role = 0;
 
