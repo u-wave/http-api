115816c04ba33f16d26792c9e1daddbdfda52f37
SooYou <souyoou@googlemail.com>
2015-10-24 06:30:00 +0200

    populate media reference

--- src/advance.js
+++ src/advance.js
@@ -1,7 +1,12 @@
+import mongoose from 'mongoose';
+
+import { GenericError } from './errors';
+
+const ObjectId = mongoose.Types.ObjectId;
+
 export default function advance(mongo, redis) {
   const Playlist = mongo.model('Playlist');
   const History = mongo.model('History');
-  const Media = mongo.model('Media');
   const User = mongo.model('User');
 
   const now = {
@@ -13,29 +18,31 @@ export default function advance(mongo, redis) {
   };
 
   return redis.lpop('waitlist')
-  .then(next => {
-    if (!next) throw new GenericError(404, 'waitlist is empty');
-    return User.findOne(ObjectId(next));
+  .then(userID => {
+    if (!userID) throw new GenericError(404, 'waitlist is empty');
+
+    return User.findOne(ObjectId(userID));
   })
   .then(user => {
     if (!user) throw new GenericError(404, 'user not found');
-    now.userID = user.id;
-    return redis.get(`playlist:{user}`);
-  })
-  .then(playlist => {
-    if (!playlist) throw new GenericError(404, 'playlist not activated');
 
-    return Playlist.findOne(ObjectId(playlist));
+    now.userID = user;
+    return redis.get(`playlist:${user.id}`);
+  })
+  .then(playlistID => {
+    if (!playlistID) throw new GenericError(404, 'playlistID not set');
+
+    return Playlist.findOne(ObjectId(playlistID), { 'media': { '$slice': [0, 1] } }).populate('media')
+    .then(playlist => {
+      if (!playlist) throw new GenericError(404, 'playlist not found');
+      return Playlist.populate(playlist, { 'path': 'media.media', 'model': 'Media' });
+    });
   })
   .then(playlist => {
     if (!playlist) throw new GenericError(404, 'playlist not found');
-    now.playlistID = playlist.id;
 
-    return Media.findOne(playlist.media[0]);
-  })
-  .then(media => {
-    if (!media) throw new GenericError(404, 'media not found');
-    now.media = media;
+    now.playlistID = playlist.id;
+    now.media = playlist.media[0];
 
     return new History({
       'user': now.userID,
@@ -44,7 +51,8 @@ export default function advance(mongo, redis) {
     }).save();
   })
   .then(history => {
-    if (!history) throw new GenericError(404, 'couldn\'t create history entry');
+    if (!history) throw new GenericError(404, 'history not found');
+
     now.historyID = history.id;
     now.played = Date.now();
     return now;
