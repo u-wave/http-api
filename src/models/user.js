e9ca092c800e5e8d8efcfb587c86e4b27e8b887b
Renée Kooi <rene@kooi.me>
2016-01-23 15:26:42 +0100

    users: fix max role in user model

--- src/models/user.js
+++ src/models/user.js
@@ -5,7 +5,7 @@ export default function (mongoose) {
     joined: { type: Date, default: Date.now },
     username: { type: String, min: 3, max: 32, required: true, unique: true },
     language: { type: String, min: 2, max: 2, default: 'en' },
-    role: { type: Number, min: 0, max: 5, default: 0 },
+    role: { type: Number, min: 0, max: 6, default: 0 },
     avatar: { type: String, min: 0, max: 256, default: '' },
     slug: { type: String, min: 3, max: 256, required: true },
     level: { type: Number, min: 0, max: 9001, default: 0 },
