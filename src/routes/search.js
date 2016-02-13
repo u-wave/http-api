86ad8af3569907607e298907f783520ff447ceeb
Renée Kooi <rene@kooi.me>
2016-02-13 17:10:03 +0100

    search: use pluggable Media Source APIs (u-wave-core#4)

--- src/routes/search.js
+++ src/routes/search.js
@@ -9,7 +9,7 @@ const log = debug('uwave:api:v1:search');
 export default function searchRoutes() {
   return router()
     .get('/', (req, res) => {
-      controller.search(req.query.query, req.uwave.config.keys)
+      controller.search(req.uwave, req.query.query)
       .then(results => res.status(200).json(results))
       .catch(e => handleError(res, e, log));
     });
