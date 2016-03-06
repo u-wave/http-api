u-wave/http-api

commit cc287ab4b6a6af17183f713269770debdd9c6ab1

sockets: use u-wave-core .publish, .subscription methods

src/controllers/authenticate.js
@@ -144,6 +144,6 @@ export function removeSession(uw, id) {
   return Authentication.findOne(new ObjectId(id)).then(auth => {
     if (!auth) throw new GenericError(404, 'user not found');
 
-    uw.redis.publish('v1p', createCommand('closeSocket', auth.id));
+    uw.publish('api-v1:socket:close', auth.id);
   });
 }

src/sockets.js
@@ -25,7 +25,7 @@ export default class WSServer {
   constructor(v1, uw, config) {
     this.v1 = v1;
     this.uw = uw;
-    this.sub = new Redis(config.redis.port, config.redis.host, config.redis.options);
+    this.sub = uw.subscription();
 
     this.wss = new WebSocket.Server({
       server: uw.server,
@@ -38,7 +38,7 @@ export default class WSServer {
     this.heartbeatInt = setInterval(this._heartbeat.bind(this), 30 * 1000);
     this.advanceTimer = null;
 
-    this.sub.on('ready', () => this.sub.subscribe('v1', 'v1p'));
+    this.sub.on('ready', () => this.sub.subscribe('v1'));
     this.sub.on('message', (channel, command) => {
       this._handleMessage(channel, command)
         .catch(e => { throw e; });
@@ -71,9 +71,7 @@ export default class WSServer {
       const historyID = await uw.redis.get('booth:historyID');
       const entry = await History.findOne({ _id: historyID });
       if (entry && `${entry.user}` === id) {
-        uw.redis.publish('v1p', createCommand('advance', {
-          remove: true
-        }));
+        uw.publish('advance', { remove: true });
       }
     };
 
@@ -266,7 +264,7 @@ export default class WSServer {
 
     if (channel === 'v1') {
       this.broadcast(command);
-    } else if (channel === 'v1p') {
+    } else if (channel === 'uwave') {
       if (_command.command === 'advance') {
         clearTimeout(this.advanceTimer);
         this.advanceTimer = null;
@@ -283,22 +281,20 @@ export default class WSServer {
           }));
 
           this.advanceTimer = setTimeout(
-            () => {
-              uw.redis.publish('v1p', createCommand('advance'));
-            },
+            () => uw.publish('advance'),
             getDuration(historyEntry.media) * 1000
           );
         } else {
           this.broadcast(createCommand('advance', null));
         }
-      } else if (_command.command === 'checkAdvance') {
+      } else if (_command.command === 'advance:check') {
         const isLocked = await uw.redis.get('waitlist:lock');
         const userRole = _command.data;
         const skipIsAllowed = !isLocked || userRole > 3;
         if (this.advanceTimer === null && skipIsAllowed) {
-          uw.redis.publish('v1p', createCommand('advance'));
+          uw.publish('advance');
         }
-      } else if (_command.command === 'closeSocket') {
+      } else if (_command.command === 'api-v1:socket:close') {
         this._close(_command.data, CLOSE_NORMAL);
       }
     }
@@ -307,7 +303,7 @@ export default class WSServer {
   destroy() {
     clearInterval(this.heartbeatInt);
     this.sub.removeAllListeners();
-    this.sub.unsubscribe('v1', 'v1p');
+    this.sub.unsubscribe('v1', 'uwave');
     this.sub.close();
     this.wss.shutdown();
   }

src/controllers/booth.js
@@ -36,7 +36,7 @@ export async function getBooth(uw) {
 
 export function skipBooth(uw, moderatorID, userID, reason) {
   uw.redis.publish('v1', createCommand('skip', { moderatorID, userID, reason }));
-  uw.redis.publish('v1p', createCommand('advance'));
+  uw.publish('advance');
   return Promise.resolve(true);
 }
 
@@ -55,7 +55,7 @@ export async function replaceBooth(uw, moderatorID, id) {
     moderatorID,
     userID: id
   }));
-  uw.redis.publish('v1p', createCommand('advance'));
+  uw.publish('advance');
   return waitlist;
 }
 

src/controllers/waitlist.js
@@ -43,7 +43,7 @@ export async function appendToWaitlist(uw, userID, forceJoin) {
     userID: user.id,
     waitlist
   }));
-  uw.redis.publish('v1p', createCommand('checkAdvance', user.role));
+  uw.publish('advance:check', user.role);
 
   return waitlist;
 }
@@ -78,7 +78,7 @@ export async function insertWaitlist(uw, moderatorID, id, position, forceJoin) {
     waitlist
   }));
 
-  uw.redis.publish('v1p', createCommand('checkAdvance', user.role));
+  uw.publish('advance:check', user.role);
 
   return waitlist;
 }

commit 998a719f39a5a7c2b4d908fba96d791f7199ab12

lint: fix issues caused by cc287ab

src/controllers/authenticate.js
@@ -4,7 +4,6 @@ import jwt from 'jsonwebtoken';
 import crypto from 'crypto';
 import debug from 'debug';
 
-import { createCommand } from '../sockets';
 import { PasswordError, TokenError, GenericError } from '../errors';
 
 const PASS_LENGTH = 256;

src/sockets.js
@@ -1,7 +1,6 @@
 import Mongoose from 'mongoose';
 import bluebird from 'bluebird';
 import jwt from 'jsonwebtoken';
-import Redis from 'ioredis';
 import WebSocket from 'ws';
 import debug from 'debug';
 
@@ -22,7 +21,7 @@ export function createCommand(command, data) {
 }
 
 export default class WSServer {
-  constructor(v1, uw, config) {
+  constructor(v1, uw) {
     this.v1 = v1;
     this.uw = uw;
     this.sub = uw.subscription();