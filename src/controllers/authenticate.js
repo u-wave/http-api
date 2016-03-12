f566dac92bfbe32154f6c2591e0140209e62902a
Renée Kooi <rene@kooi.me>
2016-03-12 11:20:33 +0100

    auth: use bcrypt instead of pbkdf2, fixes #39

--- src/controllers/authenticate.js
+++ src/controllers/authenticate.js
@@ -1,11 +1,11 @@
+import * as bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
 import Promise from 'bluebird';
 import { sign as jwtSignCallback } from 'jsonwebtoken';
-import crypto from 'crypto';
+import randomString from 'random-string';
 import debug from 'debug';
 
 import {
-  APIError,
   NotFoundError,
   PasswordError,
   PermissionError,
@@ -13,39 +13,14 @@ import {
 } from '../errors';
 import { isBanned as isUserBanned } from './bans';
 
-const PASS_LENGTH = 256;
-const PASS_ITERATIONS = 2048;
-const PASS_HASH = 'sha256';
-
 const ObjectId = mongoose.Types.ObjectId;
 const log = debug('uwave:api:v1:auth');
-const pbkdf2 = Promise.promisify(crypto.pbkdf2);
-const randomBytes = Promise.promisify(crypto.randomBytes);
+const bcryptHash = Promise.promisify(bcrypt.hash);
+const bcryptCompare = Promise.promisify(bcrypt.compare);
 // `jwt.sign` only passes a single parameter to its callback: the signed token.
 const jwtSign = (...args) => new Promise(resolve => {
   jwtSignCallback(...args, resolve);
 });
-
-export function generateHashPair(password, length) {
-  const hashPair = {
-    hash: null,
-    salt: null
-  };
-
-  return randomBytes(length)
-  .then(buf => {
-    hashPair.salt = buf.toString('hex');
-    return pbkdf2(password, hashPair.salt, PASS_ITERATIONS, length, PASS_HASH);
-  })
-  .then(salted => {
-    hashPair.hash = salted.toString('hex');
-    return hashPair;
-  })
-  .catch(e => {
-    log(e);
-    throw new APIError('Could not create password.');
-  });
-}
 
 export function getCurrentUser(uw, id) {
   const User = uw.model('User');
@@ -53,33 +28,34 @@ export function getCurrentUser(uw, id) {
   return User.findOne(new ObjectId(id));
 }
 
-export function createUser(uw, email, username, password) {
+export async function createUser(uw, email, username, password) {
   const User = uw.model('User');
   const Authentication = uw.model('Authentication');
-  let _auth = null;
 
+  let auth;
   log(`creating new user ${username}`);
   const user = new User({ username });
+  await user.validate();
 
-  return user.validate()
-  .then(() => generateHashPair(password, PASS_LENGTH))
-  .then(hashPair => {
-    _auth = new Authentication({
+  try {
+    const hash = await bcryptHash(password, 10);
+
+    auth = new Authentication({
       user: user.id,
       email,
-      hash: hashPair.hash,
-      salt: hashPair.salt
+      hash
     });
-    return _auth.save();
-  })
-  .then(() => user.save())
-  .then(() => user)
-  .catch(e => {
+
+    await auth.save();
+    await user.save();
+
+    return user;
+  } catch (e) {
     log(`did not create user ${username}. Error: ${e}`);
-    if (_auth) _auth.remove();
+    if (auth) auth.remove();
     user.remove();
     throw e;
-  });
+  }
 }
 
 export async function login(uw, email, password, options) {
@@ -90,8 +66,8 @@ export async function login(uw, email, password, options) {
     throw new NotFoundError('No user was found with that email address.');
   }
 
-  const hash = await pbkdf2(password, auth.salt, PASS_ITERATIONS, PASS_LENGTH, PASS_HASH);
-  if (hash.toString('hex') !== auth.hash) {
+  const correct = await bcryptCompare(password, auth.hash);
+  if (!correct) {
     throw new PasswordError('password is incorrect');
   }
 
@@ -100,7 +76,7 @@ export async function login(uw, email, password, options) {
   }
 
   const token = await jwtSign(
-    { id: auth.user.id, role: auth.user.role },
+    { id: auth.user.id },
     options.secret,
     { expiresIn: '31d' }
   );
@@ -111,47 +87,43 @@ export async function login(uw, email, password, options) {
   };
 }
 
-export function reset(uw, email) {
+export async function reset(uw, email) {
   const Authentication = uw.model('Authentication');
 
-  return Authentication.findOne({ email })
-  .then(auth => {
-    if (!auth) throw new NotFoundError('User not found.');
-    return randomBytes(64);
-  })
-  .then(buf => {
-    const token = buf.toString('hex');
-    uw.redis.set(`reset:${email}`, token);
-    uw.redis.expire(`reset${email}`, 24 * 60 * 60);
-    return token;
-  });
+  const auth = await Authentication.findOne({ email });
+  if (!auth) {
+    throw new NotFoundError('User not found.');
+  }
+
+  const token = randomString({ length: 35, special: false });
+
+  await uw.redis.set(`reset:${email}`, token);
+  await uw.redis.expire(`reset:${email}`, 24 * 60 * 60);
+
+  return token;
 }
 
-export function changePassword(uw, email, password, resetToken) {
+export async function changePassword(uw, email, password, resetToken) {
   const Authentication = uw.model('Authentication');
 
-  return uw.redis.get(`reset:${email}`)
-  .then(token => {
-    if (!token || token !== resetToken) {
-      throw new TokenError(
-        'That reset token is invalid. Please double-check your token or request ' +
-        'a new password reset.'
-      );
-    }
+  const token = await uw.redis.get(`reset:${email}`);
+  if (!token || token !== resetToken) {
+    throw new TokenError(
+      'That reset token is invalid. Please double-check your token or request ' +
+      'a new password reset.'
+    );
+  }
 
-    return generateHashPair(password, PASS_LENGTH);
-  })
-  .then(hashPair =>
-    Authentication.findOneAndUpdate(
-      { email },
-      { salt: hashPair.salt, hash: hashPair.hash }
-    ).exec()
-  )
-  .then(auth => {
-    if (!auth) throw new NotFoundError('No user was found with that email address.');
-    uw.redis.del(`reset:${email}`);
-    return `updated password for ${email}`;
-  });
+  const hash = await bcryptHash(password, 10);
+
+  const auth = await Authentication.findOneAndUpdate({ email }, { hash });
+
+  if (!auth) {
+    throw new NotFoundError('No user was found with that email address.');
+  }
+
+  await uw.redis.del(`reset:${email}`);
+  return `updated password for ${email}`;
 }
 
 export function removeSession(uw, id) {
