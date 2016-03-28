40c6dbd84b5b9d379191df7009c03d1b5efcbf8b
Renée Kooi <rene@kooi.me>
2016-03-28 21:24:01 +0200

    auth: rewrite authenticator middleware as async function

--- src/middleware/authenticator.js
+++ src/middleware/authenticator.js
@@ -11,38 +11,34 @@ const verify = bluebird.promisify(jwt.verify);
 const log = debug('uwave:v1:authenticator');
 
 export default function authenticatorMiddleware(v1, options) {
-  return function authenticator(req, res, next) {
+  async function authenticator(req) {
     const token = req.query && req.query.token;
     if (!token) {
-      next();
       return;
     }
 
-    verify(token, options.secret)
-    .then(user => {
-      if (!user) {
-        throw new HTTPError(404, 'user not found');
-      }
+    const user = await verify(token, options.secret);
+    if (!user) {
+      throw new HTTPError(404, 'user not found');
+    }
 
-      if (typeof user.role !== 'number') {
-        user.role = parseInt(user.role, 10);
-      }
-      user.role = clamp(user.role || 0, ROLE_DEFAULT, ROLE_ADMIN);
+    if (typeof user.role !== 'number') {
+      user.role = parseInt(user.role, 10);
+    }
+    user.role = clamp(user.role || 0, ROLE_DEFAULT, ROLE_ADMIN);
 
-      req.user = user;
-      next();
-    })
-    .catch(jwt.JsonWebTokenError, () => {
-      log(`Token '${req.query.token.slice(0, 64)}...' was not valid.`);
-      throw new HTTPError(400, 'access token invalid');
-    })
-    .catch(e => {
-      if (e instanceof HTTPError) {
-        throw e;
-      }
-      log(`Unknown error: ${e}`);
-      throw new HTTPError(500, 'internal server error, please try again later');
-    })
-    .catch(next);
+    req.user = user;
+  }
+
+  return (req, res, next) => {
+    authenticator(req)
+      .then(() => next())
+      .catch(jwt.JsonWebTokenError, error => {
+        log('invalid token', error.message);
+        throw new Error('Invalid session');
+      })
+      .catch(error => {
+        next(error);
+      });
   };
 }
