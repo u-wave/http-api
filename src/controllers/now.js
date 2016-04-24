0a5e685784c83093d8173f43c943abe032474720
Renée Kooi <rene@kooi.me>
2016-04-24 21:05:54 +0200

    playlists: move playlist logic to u-wave-core

--- src/controllers/now.js
+++ src/controllers/now.js
@@ -1,30 +1,26 @@
-import mongoose from 'mongoose';
 import Promise from 'bluebird';
 import { getBooth } from './booth';
-import { getPlaylists } from './playlists';
 import { getServerTime } from './server';
 
-const ObjectId = mongoose.Types.ObjectId;
+import { serializePlaylist } from '../utils/serialize';
 
-export async function getState(v1, uw, id) {
+export async function getState(v1, uw, user) {
   const User = uw.model('User');
 
   const guests = v1.getGuestCount();
   const motd = uw.getMotd();
-  const playlists = getPlaylists(uw, 0, 50, id);
   const booth = getBooth(uw);
-  const user = User.findOne(new ObjectId(id));
   const users = uw.redis.lrange('users', 0, -1)
     .then(userIDs => User.find({ _id: { $in: userIDs } }));
   const waitlist = uw.redis.lrange('waitlist', 0, -1);
   const waitlistLocked = uw.redis.get('waitlist:lock')
     .then(lock => lock ? true : false);
-  const activePlaylist = uw.redis.get(`playlist:${id}`);
+  const activePlaylist = user ? user.getActivePlaylistID() : null;
+  const playlists = user ? user.getPlaylists() : null;
   const time = getServerTime();
 
-  return await Promise.props({
+  const state = await Promise.props({
     motd,
-    playlists,
     booth,
     user,
     users,
@@ -32,6 +28,13 @@ export async function getState(v1, uw, id) {
     waitlist,
     waitlistLocked,
     activePlaylist,
+    playlists,
     time
   });
+
+  if (state.playlists) {
+    state.playlists = state.playlists.map(serializePlaylist);
+  }
+
+  return state;
 }
