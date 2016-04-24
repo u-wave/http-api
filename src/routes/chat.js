07dee3695e0d330d960aa545f7af46b979e264b3
Renée Kooi <rene@kooi.me>
2016-04-24 14:40:35 +0200

    chat: move chat to u-wave-core

--- src/routes/chat.js
+++ src/routes/chat.js
@@ -1,24 +1,32 @@
 import createRouter from 'router';
 
 import protect from '../middleware/protect';
-import * as controller from '../controllers/chat';
 import { ROLE_MODERATOR } from '../roles';
 
 export default function chatRoutes() {
   const router = createRouter();
 
   router.delete('/', protect(ROLE_MODERATOR), (req, res) => {
-    controller.chatDelete(req.uwave, req.user);
+    req.uwave.deleteChat(
+      {},
+      { moderator: req.user }
+    );
     res.status(200).json('deleted chat');
   });
 
   router.delete('/user/:id', protect(ROLE_MODERATOR), (req, res) => {
-    controller.chatDeleteByUser(req.uwave, req.user, req.params.id);
+    req.uwave.deleteChat(
+      { userID: req.params.id },
+      { moderator: req.user }
+    );
     res.status(200).json(`deleted chat ${req.params.id}`);
   });
 
   router.delete('/:id', protect(ROLE_MODERATOR), (req, res) => {
-    controller.chatDeleteByID(req.uwave, req.user, req.params.id);
+    req.uwave.deleteChat(
+      { id: req.params.id },
+      { moderator: req.user }
+    );
     res.status(200).json(`deleted chat by user ${req.params.id}`);
   });
 
