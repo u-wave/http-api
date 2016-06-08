10175c2dc47dc97471c5236b9f300ff5eea355ec
Renée Kooi <renee@kooi.me>
2016-06-08 12:36:56 +0200

    search: add route to search in a single source

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -1,13 +1,27 @@
 import Promise from 'bluebird';
+import createDebug from 'debug';
+import { NotFoundError } from '../errors';
 
-// eslint-disable-next-line import/prefer-default-export
-export function search(uw, query) {
+const log = createDebug('uwave:api:v1:search');
+
+export function searchAll(uw, query) {
   const promises = {};
   uw.sources.forEach((source) => {
     promises[source.type] = source.search(query)
-      // Default to empty search on failure, for now.
-      // TODO log & return error somewhere.
-      .catch(() => []);
+      .catch((error) => {
+        log(error.message);
+        // Default to empty search on failure, for now.
+        return [];
+      });
   });
+
   return Promise.props(promises);
+}
+
+export async function search(uw, sourceName, query) {
+  const source = uw.source(sourceName);
+  if (!source) {
+    throw new NotFoundError('Source not found.');
+  }
+  return await source.search(query);
 }
