f5d1239b5f59afd7dcb6bc00b99b2651257d46f2
Renée Kooi <renee@kooi.me>
2016-09-21 17:06:49 +0200

    add option to dedupe related models in json responses

--- src/utils/toPaginatedResponse.js
+++ src/utils/toPaginatedResponse.js
@@ -1,5 +1,8 @@
 import * as url from 'url';
 import * as qs from 'qs';
+import cloneDeep from 'lodash/cloneDeep';
+import getPath from 'lodash/get';
+import setPath from 'lodash/set';
 
 function appendQuery(base, query) {
   const parsed = url.parse(base, true);
@@ -11,8 +14,51 @@ function appendQuery(base, query) {
   return `${url.format(parsed)}`;
 }
 
-export default function toPaginatedResponse(page, { baseUrl = '' } = {}) {
+function extractIncluded(data, included) {
+  const includedTypes = Object.keys(included);
+  const includeds = {};
+  const had = {};
+
+  for (const typeName of includedTypes) {
+    includeds[typeName] = [];
+  }
+
+  const resultData = [];
+  for (const initialItem of data) {
+    let item = initialItem.toJSON();
+    for (const type of includedTypes) {
+      for (const path of included[type]) {
+        const includedItem = getPath(item, path);
+        if (includedItem) {
+          if (item === initialItem) {
+            item = cloneDeep(item);
+          }
+          setPath(item, path, includedItem._id);
+          if (!had[type + includedItem._id]) {
+            includeds[type].push(includedItem);
+            had[type + includedItem._id] = true;
+          }
+        }
+      }
+    }
+    resultData.push(item);
+  }
+
   return {
+    included: includeds,
+    data: resultData,
+  };
+}
+
+export default function toPaginatedResponse(
+  page,
+  { baseUrl = '', included = {} } = {}
+) {
+  let props = { data: page.data };
+  if (included) {
+    props = extractIncluded(page.data, included);
+  }
+  return Object.assign({
     meta: {
       offset: page.currentPage.offset,
       pageSize: page.pageSize,
@@ -24,6 +70,7 @@ export default function toPaginatedResponse(page, { baseUrl = '' } = {}) {
       next: appendQuery(baseUrl, { page: page.nextPage }),
       prev: appendQuery(baseUrl, { page: page.prevPage }),
     },
-    data: page.data,
-  };
+    data: null,
+    included: null,
+  }, props);
 }
