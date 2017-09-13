5d31cd0f44042f0d1a8207a218783d1fc48df616
Renée Kooi <renee@kooi.me>
2017-09-13 11:30:59 +0200

    search: Update media source API use

--- src/controllers/search.js
+++ src/controllers/search.js
@@ -6,11 +6,13 @@ import toListResponse from '../utils/toListResponse';
 const log = createDebug('uwave:api:v1:search');
 
 export function searchAll(req) {
+  const uw = req.uwave;
+  const { user } = req;
   const { query } = req.query;
   const promises = {};
 
-  req.uwave.sources.forEach((source) => {
-    promises[source.type] = source.search(query)
+  uw.sources.forEach((source) => {
+    promises[source.type] = source.search(user, query)
       .catch((error) => {
         log(error.message);
         // Default to empty search on failure, for now.
@@ -22,15 +24,17 @@ export function searchAll(req) {
 }
 
 export async function search(req) {
+  const uw = req.uwave;
+  const { user } = req;
   const sourceName = req.params.source;
   const { query } = req.query;
 
-  const source = req.uwave.source(sourceName);
+  const source = uw.source(sourceName);
   if (!source) {
     throw new NotFoundError('Source not found.');
   }
 
-  const results = await source.search(query);
+  const results = await source.search(user, query);
 
   return toListResponse(results, { url: req.fullUrl });
 }
