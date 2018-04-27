78c25d2a07d010bbbbadbcd3839ce1440488c834
Renée Kooi <renee@kooi.me>
2018-04-27 21:39:52 +0200

    shallow clone

--- demo/build.sh
+++ demo/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-git clone https://github.com/u-wave/core u-wave-core
-git clone https://github.com/u-wave/u-wave-source-youtube u-wave-source-youtube
-git clone https://github.com/u-wave/u-wave-source-soundcloud u-wave-source-soundcloud
+git clone --depth 1 https://github.com/u-wave/core u-wave-core
+git clone --depth 1 https://github.com/u-wave/u-wave-source-youtube u-wave-source-youtube
+git clone --depth 1 https://github.com/u-wave/u-wave-source-soundcloud u-wave-source-soundcloud
 
 cd u-wave-core && npm install && cd ..
 cd u-wave-source-youtube && npm install && cd ..
