b2ecefbe323ff51f0f8a7719b2a956355e7e1dc5
Renée Kooi <renee@kooi.me>
2018-04-27 20:55:21 +0200

    fix

--- demo/ci-deploy.sh
+++ demo/ci-deploy.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+if [[ $TRAVIS_NODE_VERSION != "stable" ]]; then
+  echo "not doing deploy!"
+  exit 0
+fi
 npm i -g now
 now demo -e SOUNDCLOUD_KEY -e YOUTUBE_KEY -e SECRET -e REDIS_URL -e MONGO_URL -e DEBUG --token $NOW_TOKEN --alias u-wave-demo.now.sh
