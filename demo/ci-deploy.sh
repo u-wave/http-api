c96ba86dc4f64601160c0a2a6588887a51529142
Renée Kooi <renee@kooi.me>
2018-04-27 21:46:26 +0200

    move now stuff to root dir?

--- demo/ci-deploy.sh
+++ demo/ci-deploy.sh
@@ -5,4 +5,4 @@ if [ x"$TRAVIS_NODE_VERSION" != x"stable" ]; then
   exit 0
 fi
 npm i -g now
-now demo -e SOUNDCLOUD_KEY -e YOUTUBE_KEY -e SECRET -e REDIS_URL -e MONGO_URL -e DEBUG --token $NOW_TOKEN --alias u-wave-demo.now.sh
+now -e SOUNDCLOUD_KEY -e YOUTUBE_KEY -e SECRET -e REDIS_URL -e MONGO_URL -e DEBUG --token $NOW_TOKEN --alias u-wave-demo.now.sh
