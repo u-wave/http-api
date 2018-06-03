59826b02c49886d6564d7c08c4e342cae17509b3
Renée Kooi <renee@kooi.me>
2018-06-03 13:44:53 +0200

    Use now-alias

--- demo/ci-deploy.sh
+++ demo/ci-deploy.sh
@@ -4,7 +4,7 @@ if [ x"$TRAVIS_NODE_VERSION" != x"stable" ]; then
   echo "not doing deploy!"
   exit 0
 fi
-npm i -g now
+npm i -g now now-alias
 now \
   -e SOUNDCLOUD_KEY \
   -e YOUTUBE_KEY \
@@ -15,4 +15,7 @@ now \
   --token $NOW_TOKEN \
   -p \
   -n u-wave-demo
-now alias --token $NOW_TOKEN
+now-alias \
+  --name u-wave-demo \
+  --alias u-wave-demo.now.sh \
+  --token $NOW_TOKEN
