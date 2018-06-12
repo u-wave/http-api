fea1a10defdb528b125e2252deac5c1e430bd6f8
Renée Kooi <renee@kooi.me>
2018-06-12 19:45:36 +0200

    Use joi's builtin Promise API.

--- src/middleware/checkFields.js
+++ src/middleware/checkFields.js
@@ -1,12 +1,9 @@
 import joi from 'joi';
-import { promisify } from 'util';
 import wrapMiddleware from '../utils/wrapMiddleware';
-
-const validate = promisify(joi.validate);
 
 export default function checkFields(types) {
   return wrapMiddleware(async (req) => {
-    await validate(req, types, {
+    await joi.validate(req, types, {
       abortEarly: false,
       allowUnknown: true,
     });
